5f82566daa547727487a9557a89fcb44dea4b8f5
Berke Özbek <berkeozbek540@gmail.com>
2026-04-13 19:11:14 +0300

    fix prisma

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,7 @@
 import { PrismaClient } from "../src/generated/prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 export const db = globalForPrisma.prisma ?? new PrismaClient();
 
